e3fb33a6a159ad9b962018315961ea9c89b067fd
udud0099 <udud0099@gmail.com>
2025-09-18 11:52:32 +0545

    update

---
+++ classsss/step3vite/shaders/fragment.glsl
@@ -0,0 +1,6 @@
+uniform sampler2D uImage
+varying vec2 vUv
+
+void main(){
+    gl_FragColor = texture2D(uImage, vUv)
+}
--- classsss/step3vite/src/main.js
+++ classsss/step3vite/src/main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import vertex from './../shaders/vertex.glsl'
+import fragment from './../shaders/fragment.glsl'
 
 class Site {
   constructor({ dom }) {
@@ -16,31 +18,59 @@ class Site {
     this.camera = new THREE.PerspectiveCamera(
       75,
       this.width / this.height,
-      0.1,
-      1000
+      100,
+      2000
     );
-    this.camera.position.z = 5;
+    this.camera.position.z = 200;
+    this.camera.fov = 2 * Math.atan(this.height / 2 / 200) * (180 / Math.PI);
 
     this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
 
-    this.addObjects();
+    // this.addObjects();
+
+    this.renderer.render(this.scene, this.camera);
+
+    this.addImages();
 
     this.render();
   }
+  addImages() {
+    const textureLoader = new THREE.TextureLoader();
+    const textures = this.images.map((img) => textureLoader.load(img));
 
-  addObjects() {
-    this.geometry = new THREE.BoxGeometry(1, 1, 1);
-    this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    this.cube = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.cube);
+    const uniforms = {
+      uTime: { value: 0 },
+      uImage: { value: textures[0] },
+    };
+
+    this.material = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: vertex,
+      fragmentShader: fragment,
+      transparent: true,
+    });
+
+    this.images.forEach((img) => {
+      const bounds = img.getBoundingClientRect();
+      const geometry = new THREE.PlaneGeometry(bounds.width, bounds.height);
+      const mesh = new THREE.Mesh(geometry, this.material);
+
+      this.scene.add(mesh);
+
+      this.imagesStore.push({
+        img: img,
+        mesh: mesh,
+        top: bounds.top,
+        left: bounds.left,
+        width: bounds.width,
+        height: bounds.height,
+      });
+    });
   }
-
   render() {
     this.time++;
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
     this.renderer.render(this.scene, this.camera);
     window.requestAnimationFrame(this.render.bind(this));
     // console.log(this.time);
